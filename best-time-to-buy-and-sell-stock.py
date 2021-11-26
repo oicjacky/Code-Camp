3d7927bbd6fd38b56cb75f950f3614fd9f7498fd
oicjacky <610611105@gms.ndhu.edu.tw>
2021-11-26 15:10:22 +0800

    New: 121. Best Time to Buy and Sell Stock

---
+++ best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,50 @@
+''' 
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ 
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+ 
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+'''
+from typing import List
+
+class Solution:
+
+    #NOTE: Time Limit Exceeded
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for buy_id in range(len(prices)):
+            for sell_id in range(buy_id, len(prices)):
+                earn = prices[sell_id] - prices[buy_id]
+                if earn > profit:
+                    profit = earn
+        return profit
+    
+    #NOTE: O(n) the same concept as Maximum subarray (1188 ms	25.1 MB)
+    def maxProfit(self, prices: List[int]) -> int:
+        min_so_far = prices[0]
+        earn_so_far = 0
+        for i in range(1, len(prices)):
+            earn_so_far = max(prices[i] - min_so_far, earn_so_far)
+            min_so_far = min(min_so_far, prices[i])
+            #print(min_so_far, earn_so_far)
+        return earn_so_far
+
+if __name__ == '__main__':
+
+    print(Solution().maxProfit([7,1,5,3,6,4]))
+    print(Solution().maxProfit([7,6,4,3,1]))
