07303bc724b6cfa7b51375a1157de81c4ae23f2d
oicjacky <610611105@gms.ndhu.edu.tw>
2021-12-05 17:20:30 +0800

    Add another answer and some comments of the algorithom

--- word-pattern.py
+++ word-pattern.py
@@ -29,29 +29,46 @@ s does not contain any leading or trailing spaces.
 All the words in s are separated by a single space.
 '''
 class Solution:
+
+    #NOTE: O(N + set of dict) (PR83 28 ms	14.3 MB)
     def wordPattern(self, pattern: str, s: str) -> bool:
-        patter2s = {}
+        ''' Implement a 1-1 function between `pattern` and `s`.
+        1. create a dictionary `pattern2s`
+        2. Not one-to-one, means that it occurs the following situations:
+            i. one `pattern` corresponds to two value in `s`
+            ii. many `pattern` correspond to one value in `s`
+        3. Not onto i.e. length of patterns not equals to length of words
+        '''
+        pattern2s = {}
         words = s.split(' ')
-        if len(words) != len(pattern): return False
-        for p, word in zip(pattern, words):
-            if p not in patter2s:
-                patter2s[p] = word
-            else:
-                if patter2s[p] != word:
-                    print(patter2s, 'occur value collision', word)
-                    return False
-        # prevent many-to-one
-        if len(set(patter2s.keys())) != len(set(patter2s.values())):
-            print(set(patter2s.keys()), set(patter2s.values()))
+        if len(words) != len(pattern): 
             return False
-        print(patter2s)
+        for p, word in zip(pattern, words):
+            if p not in pattern2s:
+                pattern2s[p] = word
+            else:
+                if pattern2s[p] != word:
+                    print('[False]', pattern2s, 'occur value collision {', p, ':', word, '}')
+                    return False
+        if len(set(pattern2s.keys())) != len(set(pattern2s.values())):
+            print('[False]', set(pattern2s.keys()), set(pattern2s.values()))
+            return False
+        print('[True]', pattern2s)
         return True
 
+    #NOTE: more clear, more faster (PR95 24 ms	14.4 MB)
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        '''Use `set(zip(pattern, words))` to satisfy 2 above.'''
+        words = s.split(' ')
+        print(set(zip(pattern, words)))
+        return len(pattern) == len(words) and \
+                len(set(pattern)) == len(set(words)) == len(set(zip(pattern, words)))
 
 
 if __name__ == '__main__':
 
     print(Solution().wordPattern("abba", "dog cat cat dog")) #T
     print(Solution().wordPattern("abba", "dog cat cat fish")) #F
-    print(Solution().wordPattern("abba", "dog dog dog dog")) #many-to-one
+    print(Solution().wordPattern("abba", "dog dog dog dog")) #F many-to-one
     print(Solution().wordPattern("aaa", "aa aa aa aa")) #F
+    print(Solution().wordPattern("aba", "dog cat cat")) #F
